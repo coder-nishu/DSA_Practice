08df13066e9e7c41172661e02b981cce4baac5ac
coder-nishu <nishatkh1582@gmail.com>
2023-08-05 00:31:29 +0600

    binary & linear done

--- Algorithm/linear_binary_search.c/linear_search.c
+++ Algorithm/linear_binary_search.c/linear_search.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int linear_search(int arr[], int size, int key);
-int Binary_search(int arr[], int size, int key);
+
 
 int linear_search(int arr[], int size, int key)
 {
@@ -13,26 +13,7 @@ int linear_search(int arr[], int size, int key)
      }
      return -1;
 }
-int Binary_search(int arr[], int size, int key)
-{
-     int low, mid, high;
-     low = 0;
-     high = size - 1;
-     mid = (low + high) / 2;
-     while (low <= mid)
-     {
 
-          if (arr[mid] == key)
-          {
-               return mid;
-          }
-          else if (arr[mid] < key)
-               low = mid + 1;
-          else
-               high = mid - 1;
-     }
-     return -1;
-}
 int main()
 {
      int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
@@ -41,7 +22,7 @@ int main()
      printf("Enter the search key:");
      scanf("%d", &key);
      int l = linear_search(arr, size, key);
-     int b = Binary_search(arr, size, key);
+   
      if (l == -1)
      {
           printf("Linear search: no search found\n");
@@ -50,14 +31,6 @@ int main()
      {
           printf("Linear search:The element %d is found at index %d\n", key, l);
      }
-     printf("\n");
-     if (b == -1)
-     {
-          printf("Binary search: no search found\n");
-     }
-     else
-     {
-          printf("Binary search:The element %d is found at index %d\n", key, b);
-     }
+     
      return 0;
 }
